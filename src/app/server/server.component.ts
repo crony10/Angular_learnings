421a8f02a989ee730a3123778e4e93e8f31a42d4
Crony <cronypaul@gmail.com>
2022-01-11 16:00:45 +0530

    styling the contents of components

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,8 +1,14 @@
+import { style } from "@angular/animations";
 import { Component } from "@angular/core";
 
 @Component({
     selector:'app-server',
-    templateUrl:'./server.component.html'
+    templateUrl:'./server.component.html',
+    styles:[`
+    p{
+        color: green;
+    }
+    `]
 })
 export class ServerComponent{
 
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,9 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   template: `
+  <p>Servers component called</p>
   <app-server></app-server>
   <app-server></app-server>`,
-  styleUrls: ['./servers.component.css']
+  styles: [`
+  p{
+    color: blue;
+  }
+  `]
 })
 export class ServersComponent implements OnInit {
 
