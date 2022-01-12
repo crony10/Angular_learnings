53f3db8db466cdc47731268b95990650520150eb
Crony <cronypaul@gmail.com>
2022-01-12 18:54:18 +0530

    1st method of databinding : string interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,12 +4,15 @@ import { Component } from "@angular/core";
 @Component({
     selector:'app-server',
     templateUrl:'./server.component.html',
-    styles:[`
-    p{
-        color: green;
-    }
-    `]
+    styleUrls:[`./server.component.css`]
 })
 export class ServerComponent{
+    serverNumber: number = 10;
 
+    serverStatus: string = 'offline';
+    
+    fun(){
+        var text = 'returned some text';
+        return text;
+    }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,24 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  // this selector is selected by the element
   selector: 'app-servers',
-
-  // this selector is selected by the attribute
-  // selector: '[app-servers]',
-
-  // this selector  is selected by the class
-  // selector: '.app-servers',
-
-  template: `
-  <p>Servers component called</p>
-  <app-server></app-server>
-  <app-server></app-server>`,
-  styles: [`
-  p{
-    color: blue;
-  }
-  `]
+  templateUrl: `./servers.component.html`,
+  styleUrls: [`./servers.component.css`]
 })
 export class ServersComponent implements OnInit {
 
