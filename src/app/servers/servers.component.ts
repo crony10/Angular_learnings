88554590b9fa63d90e4a643fb648cccbc826db6e
Crony <cronypaul@gmail.com>
2022-01-20 17:25:21 +0530

    ngFor

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,7 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+    serverCreationStatus = 'Server is not created yet';
+    serverName =  'server initial';
+    serverCreated = false;
+    servers = ['server 1','server 2'];
+    
+
+    constructor(){
+        setTimeout(() => {
+            this.allowNewServer = true;
+        }, 2000);
+    }
+
+    onCreateServer(){
+        this.serverCreated = true;
+        this.servers.push(this.serverName);
+        this.serverCreationStatus = 'Server is created and name of the server is ' + this.serverName + ' wohooooooo';
+    }
+
+    onUpdateServer(event: Event){
+        this.serverName = (<HTMLInputElement>event.target).value;
+    }
 
   ngOnInit(): void {
   }
