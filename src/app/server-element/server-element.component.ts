2869606764a6e44b9a0fdbbf9180e7d22109ddac
Crony <cronypaul@gmail.com>
2022-02-12 12:09:04 +0530

    view encapsulation in css

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+
+  // 1st option:emulated and 2nd option:shadowDom both are same and the default behaviour.
+  // However, shadowDom uses browser's technology while emulated uses angular's built in technology 
+  // 3rd option: none =>which applies the style globally
+  // https://angular.io/guide/view-encapsulation
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class ServerElementComponent implements OnInit {
 
   // This is example of binding to custom properties
   // 'srvElement' is the alias given to the element which is being used in app component.html
-  @Input('srvElement') element!: {type:string,name:string,content: string};
+  @Input('srvElement') element: {type:string,name:string,content: string};
 
   constructor() { }
 
